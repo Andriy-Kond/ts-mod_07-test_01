6394793bdea67305710b779b55c5a3a869dfa20c
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-07-06 17:21:32 +0300

    upd: app

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,40 @@
-import React, { useState } from "react";
-import AddTodoOnChange from "./components/AddTodoOnChange";
-import AddTodoRef from "./components/AddTodoRef";
-import TodoList from "./components/TodoList";
-import { IItem } from "./types/todo";
+import React, { useState } from 'react';
+import AddTodoOnChange from './components/AddTodoOnChange';
+import AddTodoRef from './components/AddTodoRef';
+import TodoList from './components/TodoList';
+import { IItem } from './types/todo';
 
 const App: React.FC = () => {
-	const [todos, setTodos] = useState<IItem[]>([]);
+  const [todos, setTodos] = useState<IItem[]>([]);
 
-	// обробники
-	function todoAddHandler(todo: IItem) {
-		setTodos((prevTodos) => {
-			return [
-				...prevTodos,
-				{
-					id: Math.random().toString(),
-					title: todo.title,
-				},
-			];
-		});
-	}
+  // обробники
+  function todoAddHandler(todo: IItem) {
+    setTodos(prevTodos => {
+      return [
+        ...prevTodos,
+        {
+          id: Math.random().toString(),
+          title: todo.title,
+        },
+      ];
+    });
+  }
 
-	function todoRemoveHandler(id: string): void {
-		setTodos((prevTodos) => {
-			return prevTodos.filter((item) => {
-				return item.id !== id;
-			});
-		});
-	}
+  function todoRemoveHandler(id: string): void {
+    setTodos(prevTodos => {
+      return prevTodos.filter(item => {
+        return item.id !== id;
+      });
+    });
+  }
 
-	return (
-		<div className="App">
-			<AddTodoOnChange onAddTodo={todoAddHandler} />
-			<AddTodoRef />
-			<TodoList todos={todos} onRemoveTodo={todoRemoveHandler} />
-		</div>
-	);
+  return (
+    <div className="App">
+      <AddTodoOnChange onAddTodo={todoAddHandler} />
+      <AddTodoRef />
+      <TodoList todos={todos} onRemoveTodo={todoRemoveHandler} />
+    </div>
+  );
 };
 
 export default App;
--- src/App_01.tsx
+++ src/App_01.tsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react';
 // import AddTodoOnChange from "./components/AddTodoOnChange";
-import AddTodoRef from "./components/AddTodoRef";
-import TodoList from "./components/TodoList";
+import AddTodoRef from './components/AddTodoRef';
+// import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
-	const todos = [{ id: "some id number 1", title: "some text" }];
-	return (
-		<div className="App">
-			{/* <TodoList todos={todos} /> */}
-			<AddTodoRef />
-			{/* <AddTodoOnChange /> */}
-		</div>
-	);
+  // const todos = [{ id: "some id number 1", title: "some text" }];
+  return (
+    <div className="App">
+      {/* <TodoList todos={todos} /> */}
+      <AddTodoRef />
+      {/* <AddTodoOnChange /> */}
+    </div>
+  );
 };
 
 export default App;
